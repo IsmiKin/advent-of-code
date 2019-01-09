2be42ec80dcbd3be6a73a78ee269ebe80de36895
Ismael Ors <ismikin@gmail.com>
2019-01-09 16:35:45 +0100

    Detected first frequency repeated twice. #Part2

--- advent_of_code.py
+++ advent_of_code.py
@@ -1,10 +1,26 @@
 
 from input_data import INPUT_DATA
 
+from collections import Counter
 
-# Part 1
+# Part 2
+
+# Counter collection class is used because its a dict subclass
+# for counting hashable objects. In other languages i would use just
+# a normal dictionary
+
 current_frequency = 0
-for number in INPUT_DATA:
-    current_frequency += int(number)
 
-print(current_frequency)
+frequency_records = Counter([current_frequency])
+first_frequency_repeated_twice = None
+
+while not first_frequency_repeated_twice:
+    # Time complexity: O(n)
+    for frequency in INPUT_DATA:
+        current_frequency += int(frequency)
+        frequency_records.update([current_frequency])
+        if frequency_records[current_frequency] == 2:
+            first_frequency_repeated_twice = current_frequency
+            break
+
+print(first_frequency_repeated_twice)
